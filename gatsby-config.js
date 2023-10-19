53a77940ad3e13c3615c74d3f2456d2c4dd4fdce
Dasun Bandara <dasun@eight25media.com>
2023-10-19 18:14:43 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GatsbyRoadMap`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
